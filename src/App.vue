8c3b8972d6e68f29d17b2c09fb8eb3da383bc624
anovau <novau_678@hotmail.com>
2022-10-18 12:09:01 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,22 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import HelloWorld from './components/HelloWorld.vue'
-console.log(process.env.NODE_ENV)
-</script>
-
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <h1>Task App</h1>
+  <ul>
+    <li>
+      <router-link :to="{name: 'home'}">Home</router-link>
+    </li>
 
+    <li>
+      <router-link :to="{name: 'login'}">Login</router-link>
+    </li>
+  </ul>
+  <router-view></router-view>
+</template>
+<script setup>
+import { ref } from 'vue';
+
+console.log(process.env.NODE_ENV)
+console.log(import.meta.env.BASE_URL)
+
+</script>
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
